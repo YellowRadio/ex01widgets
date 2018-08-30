a18f548d3d0c6e9cdfdc8043e2e9ec6ffad97d16
PC da Fatec <vinicius.araki@fatecitapetininga.edu.br>
2018-08-29 22:14:58 -0300

    initial commit

--- app/src/main/java/com/example/desenvolvedor/ex01/MainActivity.java
+++ app/src/main/java/com/example/desenvolvedor/ex01/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.desenvolvedor.ex01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public Spinner jspOperacoes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        jspOperacoes=(Spinner)findViewById(R.id.spinnerOperacoes);
+        jspOperacoes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Integer opcao = (int) jspOperacoes.getSelectedItemId();
+
+                if(opcao ==0){
+                    Toast.makeText(getApplicationContext(),"somar",Toast.LENGTH_LONG).show();
+                }else if(opcao ==1){
+                    Toast.makeText(getApplicationContext(),"subtrair",Toast.LENGTH_LONG).show();
+                }
+            }
+            public void onNothingSelected(AdapterView<?> parent){
+
+            }
+        });
     }
 }
